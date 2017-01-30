3a38b108de97197e652b6310318cc8bc4122ed84
Adrian Buzatu <adrian.buzatu@cern.ch>
2017-01-30 17:28:05 +0200

    Update update.sh

--- util/update.sh
+++ util/update.sh
@@ -2,6 +2,10 @@ git pull origin master
 for FOLDER in Buzatu*; do
     echo $FOLDER
     cd $FOLDER
+    git status
+    git fetch
+    git status
     git pull origin master
+    git status
     cd ..
 done
