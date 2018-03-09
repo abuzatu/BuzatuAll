d0028d7e454438c3f8082809d52a4ba6c9415e64
Adrian Buzatu <adrian.buzatu@cern.ch>
2018-03-09 22:03:26 +0100

    If for login or non-login shell

--- util/setup.sh
+++ util/setup.sh
@@ -17,5 +17,15 @@ export Python="${All}/BuzatuPython"
 export VH="${All}/BuzatuVH"
 export LD_LIBRARY_PATH="${Tree}/lib:${LD_LIBRARY_PATH}"
 export PYTHONPATH=${Python}:${WH}/config:${WH}/helper:$PYTHONPATH
-echo "New LD_LIBRARY_PATH=${LD_LIBRARY_PATH}"
-echo "New PYTHONPATH=${PYTHONPATH}"
+test_login_shell=$(shopt | grep login_shell)
+test_login_shell_result=`echo ${test_login_shell} | cut -d" " -f2`
+
+if [ ${test_login_shell_result} == "on" ]; then
+    echo 'Login shell'
+    echo "New LD_LIBRARY_PATH=${LD_LIBRARY_PATH}"
+    echo "New PYTHONPATH=${PYTHONPATH}"
+else 
+    :
+    #echo 'Not login shell' but it would not continue to do a scp if we use echo
+fi
+
