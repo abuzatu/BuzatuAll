fa41ec2c89c8d6181312584d33a8ed696927aa0b
Adrian Buzatu <adrian.buzatu@cern.ch>
2019-05-28 14:47:34 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 CODE_STEM="/data06/abuzatu/code"
 DATA_STEM="/data06/abuzatu/data"
-CODE_CXAOD="CxAODFramework_tag_r32-07"
-# CODE_CXAOD="CxAODFramework_branch_master_21.2.60_1"
-DATA_CXAOD=
+#CODE_CXAOD="CxAODFramework_tag_r32-07"
+CODE_CXAOD="CxAODFramework_branch_master_21.2.60_11"
+#CODE_CXAOD="CxAODFramework_branch_master_21.2.61_1"
+DATA_CXAOD="190208_32-07"
 cd ${CODE_STEM}/${CODE_CXAOD}/run
 source ../source/CxAODOperations_VHbb/scripts/setupLocal.sh
 source ../source/CxAODOperations_VHbb/scripts/setupGrid.sh Higgs 1
